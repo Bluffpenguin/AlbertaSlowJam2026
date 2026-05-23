91424dfcbc1ca8e53e2b230863fefcdd4bd3657e
JDoddsNAIT <144161210+JDoddsNAIT@users.noreply.github.com>
2026-05-22 21:05:40 -0600

    Created project assemblies

---
+++ Assets/Scripts/Core/AssemblyInfo.cs
@@ -0,0 +1,14 @@
+// Global Usings
+global using System;
+global using System.Collections;
+global using System.Collections.Generic;
+global using UnityEngine;
+global using Object = UnityEngine.Object;
+global using Random = UnityEngine.Random;
+// Internal Assemblies
+[assembly: System.Runtime.CompilerServices.InternalsVisibleTo("Scripts.Editor")]
+// Enables use of the 'init' keyword and primary constructors for records.
+namespace System.Runtime.CompilerServices
+{
+    internal sealed class IsExternalInit { }
+}
---
+++ Assets/Scripts/WebGL/AssemblyInfo.cs
@@ -0,0 +1,12 @@
+// Global Usings
+global using System;
+global using System.Collections;
+global using System.Collections.Generic;
+global using UnityEngine;
+global using Object = UnityEngine.Object;
+global using Random = UnityEngine.Random;
+// Enables use of the 'init' keyword and primary constructors for records.
+namespace System.Runtime.CompilerServices
+{
+    internal sealed class IsExternalInit { }
+}
