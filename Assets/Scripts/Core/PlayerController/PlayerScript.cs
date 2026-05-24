d9d5b6d0e2fde269bc7d755a4bc1bff1b16b0b43
DylanDoty <dylandoty87@gmail.com>
2026-05-24 16:34:23 -0600

    Player Movement done

--- Assets/Scripts/Core/PlayerController/PlayerScript.cs
+++ Assets/Scripts/Core/PlayerController/PlayerScript.cs
@@ -2,15 +2,33 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    private InputSystem_Actions _playerInput;
+    [SerializeField] private float _moveSpeed = 10;
+    private Vector2 _moveDir;
+    private Rigidbody2D _rb;
+
+
+	private void Awake()
+	{
+        _playerInput = new();
+        _rb = GetComponent<Rigidbody2D>();
+	}
+
+	private void OnEnable()
+	{
+		_playerInput.Enable();
+	}
+	private void OnDisable()
+	{
+		_playerInput.Disable();
+	}
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
-    }
+        _moveDir = _playerInput.Player.Move.ReadValue<Vector2>();
+		Vector2 displacement = _moveSpeed * Time.fixedDeltaTime * _moveDir;
+
+		_rb.AddForce(displacement,ForceMode2D.Impulse);
+	}
 }
