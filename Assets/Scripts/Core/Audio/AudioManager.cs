fffe58e07afc0350da8f45a5889b30ca94fc0bcb
DylanDoty <dylandoty87@gmail.com>
2026-06-01 17:48:55 -0600

    Started scripting Audio manager and creating manager object

--- Assets/Scripts/Core/Audio/AudioManager.cs
+++ Assets/Scripts/Core/Audio/AudioManager.cs
@@ -3,15 +3,23 @@ using FMODUnity;
 
 public class AudioManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    public static AudioManager Instance { get; private set; }
 
-    // Update is called once per frame
-    void Update()
-    {
-		
+	private void Awake()
+	{
+		if (Instance != null && Instance != this)
+		{
+			Destroy(this);
+		}
+		else
+		{
+			Instance = this;
+			DontDestroyOnLoad(Instance);
+		}
+	}
+
+	public void PlayOneShot(EventReference sound, Vector3 worldPos)
+	{
+		RuntimeManager.PlayOneShot(sound, worldPos);
 	}
 }
