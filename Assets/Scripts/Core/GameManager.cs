e8c707f1ab0c9a3f63b861f1e04bf5c8e545cf0e
Malachi Munro <98977314+Bluffpenguin@users.noreply.github.com>
2026-06-03 18:22:12 -0600

    Adde lose state and transitions

--- Assets/Scripts/Core/GameManager.cs
+++ Assets/Scripts/Core/GameManager.cs
@@ -9,11 +9,21 @@ public class GameManager : MonoBehaviour
 	[SerializeField] private string _hudScene = "HUD";
 
 	[Header("Game Settings")]
+	[SerializeField] public bool AdvanceClockInShip = true;
 	[Range(1, 12)] public int WakeUpHour = 6;
 	[Range(13, 24)] public int EndOfDayHour = 18;
 	[Tooltip("The ratio between realtime seconds to in-game minutes.")]
 	public float SecondsPerMinute = 1;
 	[SerializeField] private int[] _quotas = new int[1] { 40 };
+
+	enum GameState
+	{
+		InDungeon,
+		InShip,
+		EndScreen
+	}
+	[SerializeField] GameState gameState = GameState.InShip;
+	bool dayStarted = false;
 
 	[Header("Dynamic Data")]
 	public int DayIndex = -1;
@@ -46,6 +56,9 @@ public class GameManager : MonoBehaviour
 		Instance = this;
 		this.transform.SetParent(null);
 		DontDestroyOnLoad(this.gameObject);
+
+		TransitionManager.Instance.TransitionToShip.AddListener(SwitchToShip);
+		TransitionManager.Instance.TransitionToDungeon.AddListener(SwitchToDungeon);
 	}
 
 	private void Start()
@@ -56,14 +69,23 @@ public class GameManager : MonoBehaviour
 
 	public void Update()
 	{
-		if (!AdvanceClock) {
+		if (!AdvanceClock || !dayStarted) {
 			return;
 		}
+
+		if (!AdvanceClockInShip && gameState == GameState.InShip) return;
 
 		TimeElapsed += Time.deltaTime;
 		var minutesPassed = TimeElapsed / SecondsPerMinute;
 		CurrentHour = WakeUpHour + (minutesPassed / 60);
 		CurrentMinute = minutesPassed % 60;
+
+		if (CurrentHour >= EndOfDayHour)
+		{
+			AdvanceClock = false;
+			EndDay();
+		}
+
 	}
 
 	public async void StartGame()
@@ -79,6 +101,21 @@ public class GameManager : MonoBehaviour
 	{
 		DayIndex = -1;
 		TimeElapsed = 0;
+		gameState = GameState.InShip;
+	}
+
+	public void EndDay()
+	{
+		if (PlayerMoney >= _quotas[DayIndex] && gameState == GameState.InShip)
+		{
+			// Passed, proceed to next day
+			TransitionManager.Instance.SleepToNextDay.Invoke();
+		}
+		else
+		{
+			// Failed quota
+			EndGame();
+		}
 	}
 
 	public void MoveToNextDay()
@@ -88,6 +125,7 @@ public class GameManager : MonoBehaviour
 		DayIndex++;
 		TimeElapsed = 0;
 		PlayerMoney = 0;
+		dayStarted = false;
 
 		if (DayIndex >= DaysToWin) {
 			EndGame();
@@ -103,8 +141,25 @@ public class GameManager : MonoBehaviour
 		AdvanceClock = true;
 	}
 
+	void SwitchToDungeon() 
+	{
+		gameState = GameState.InDungeon;
+		dayStarted = true;
+	}
+	void SwitchToShip() {  gameState = GameState.InShip; }
+
 	public void EndGame()
 	{
+		Time.timeScale = 0;
+		
+		if (gameState == GameState.InDungeon)
+		{
+			TransitionManager.Instance.GameOver.Invoke("Abandoned");
+		}
+		else { TransitionManager.Instance.GameOver.Invoke("Unemployed"); }
+
+		gameState = GameState.EndScreen;
+
 		Debug.Log("Game has ended");
 	}
 }
--- Assets/Scripts/Core/Transition/TransitionManager.cs
+++ Assets/Scripts/Core/Transition/TransitionManager.cs
@@ -1,4 +1,5 @@
 using System.Linq;
+using TMPro;
 using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.SceneManagement;
@@ -13,8 +14,15 @@ public class TransitionManager : MonoBehaviour
     internal UnityEvent MidShipTransition = new UnityEvent();
 	internal UnityEvent MidDungeonTransition = new UnityEvent();
 	internal UnityEvent EndTransition = new UnityEvent();
+    internal UnityEvent<string> GameOver = new UnityEvent<string>();
+	internal UnityEvent SleepToNextDay = new UnityEvent();
 
     [SerializeField] private Image fadeToBlack;
+
+    [Header("Game Over Objs")]
+    [SerializeField] private Image gameOverScreen;
+    [SerializeField] private GameObject gameOverButtons;
+	[SerializeField] private TextMeshProUGUI gameOverText;
 
     Transform shipPlayer;
     Transform dungeonPlayer;
@@ -32,9 +40,13 @@ public class TransitionManager : MonoBehaviour
         DontDestroyOnLoad(gameObject);
         TransitionToShip.AddListener(ToShip);
         TransitionToDungeon.AddListener(ToDungeon);
+		GameOver.AddListener(OnGameOver);
+		SleepToNextDay.AddListener(OnSleep);
 	}
 	void Start()
     {
+        gameOverScreen.gameObject.SetActive(false);
+        gameOverButtons.SetActive(false);
         dungeonPlayer = shipPlayer = Player.Instance.transform;
 
         sceneCamera = Camera.main;
@@ -48,14 +60,17 @@ public class TransitionManager : MonoBehaviour
 	}
 
     void ToDungeon()
-    {
-        
+    {       
 			StartCoroutine(TransportToDungeon(3));
-        
-        
     }
 
-    void MoveCamera(Transform player)
+	void OnSleep() { StartCoroutine(Sleep()); }
+	void OnGameOver(string message) { StartCoroutine(GameOverTransition(message)); }
+
+	public void OnGameOverRestartClicked() { StartCoroutine(GameOverRestart()); }
+	public void OnGameOverExitClicked() { return; }
+
+	void MoveCamera(Transform player)
     {
         sceneCamera.transform.SetParent(player);
         sceneCamera.transform.localPosition = cameraLocalPos;
@@ -97,6 +112,7 @@ public class TransitionManager : MonoBehaviour
 
 	IEnumerator TransportToDungeon(float time)
 	{
+		
 		Player.Controller.PlayerInput.Disable();
 		fadeToBlack.enabled = true;
         BeginTransition.Invoke();
@@ -130,6 +146,97 @@ public class TransitionManager : MonoBehaviour
 
 		EndTransition.Invoke();
 		
+		fadeToBlack.enabled = false;
+	}
+
+    IEnumerator GameOverTransition(string gameOverMessage)
+    {
+
+		// Ensure the screen and text is transparent
+		Color transparent = gameOverScreen.color;
+        transparent.a = 0;
+        gameOverScreen.color = transparent;
+		transparent = gameOverText.color;
+		transparent.a = 0;
+		gameOverText.color = transparent;
+
+		Player.Controller.PlayerInput.Disable();
+		gameOverScreen.gameObject.SetActive(true);
+		gameOverText.text = gameOverMessage;
+		gameOverButtons.SetActive(false);
+		while (gameOverScreen.color.a < 1 || gameOverText.color.a < 1)
+		{
+			Color screenColor = gameOverScreen.color;
+			screenColor.a += 1 * Time.unscaledDeltaTime;
+			gameOverScreen.color = screenColor;
+
+			Color textColor = gameOverText.color;
+			textColor.a += 1 * Time.unscaledDeltaTime;
+			gameOverText.color = textColor;
+			yield return null;
+		}
+
+        gameOverButtons.SetActive(true);
+	}
+
+    IEnumerator GameOverRestart()
+    {
+		Color whole = fadeToBlack.color;
+		whole.a = 1;
+		fadeToBlack.color = whole;
+
+		fadeToBlack.enabled = true;
+		gameOverButtons.SetActive(false);
+		gameOverScreen.gameObject.SetActive(false);
+
+		MidShipTransition.Invoke();
+		dungeonPlayer.gameObject.SetActive(false);
+		Player.Controller.transform.position = Vector3.zero; // Hacky
+		GameManager.Instance.ResetGame();
+		GameManager.Instance.MoveToNextDay();
+
+		shipPlayer.gameObject.SetActive(true);
+		MoveCamera(shipPlayer);
+	
+		Player.Controller.PlayerInput.Enable();
+		Time.timeScale = 1;
+		while (fadeToBlack.color.a > 0)
+		{
+			Color color = fadeToBlack.color;
+			color.a -= (1) * Time.deltaTime;
+			fadeToBlack.color = color;
+			yield return null;
+		}
+
+		EndTransition.Invoke();
+		fadeToBlack.enabled = false;
+	}
+
+	IEnumerator Sleep()
+	{
+		Player.Controller.PlayerInput.Disable();
+		fadeToBlack.enabled = true;
+		BeginTransition.Invoke();
+		while (fadeToBlack.color.a < 1)
+		{
+			Color color = fadeToBlack.color;
+			color.a += 0.5f * Time.deltaTime;
+			fadeToBlack.color = color;
+			yield return null;
+		}
+
+		GameManager.Instance.MoveToNextDay();
+		Player.Controller.PlayerInput.Enable();
+		while (fadeToBlack.color.a > 0)
+		{
+			Color color = fadeToBlack.color;
+			color.a -= 0.5f * Time.deltaTime;
+			fadeToBlack.color = color;
+			yield return null;
+		}
+
+		EndTransition.Invoke();
+
 		fadeToBlack.enabled = false;
 	}
 }
